4605073866c0a574847d24e9b654c454290ec9cc
Harry <ex556615@gmail.com>
2025-08-19 10:29:21 +0800

    CRUD路由規則

--- project_f/server/index.js
+++ project_f/server/index.js
@@ -1,6 +1,6 @@
 // 測試.env是否有抓取到
 // console.log(process.env.JWT_SECRET_KEY);
-import express from "express";
+import express, { json } from "express";
 import multer from "multer";
 import cors from "cors";
 import moment from "moment";
@@ -28,6 +28,51 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("首頁");
+});
+
+// 獲取所有使用者
+app.get("/api/users", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: [],
+  });
+});
+
+// 獲取特定 ID 的使用者
+app.get("/api/users/:id", (req, res) => {
+  // 路由參數
+  const id = req.params.id;
+  res.status(200).json({
+    status: "success",
+    data: { id }, // {id: id}(不省略版),
+  });
+});
+
+// 新增一個使用者
+app.post("/api/users", (req, res) => {
+  res.status(201),
+    json({
+      status: "success",
+      data: {},
+    });
+});
+
+// 更新(特定 ID)的使用者
+app.put("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  res.status(200).json({
+    status: "success",
+    data: { id },
+  });
+});
+
+// 刪除(特定 ID)使用者
+app.delete("/api/user/:id", (req, res) => {
+  const id = req.params.id;
+  res.status(200).json({
+    status: "success",
+    data: { id },
+  });
 });
 
 app.listen(3005, () => {
