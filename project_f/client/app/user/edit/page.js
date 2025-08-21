d2e8b72812fa191ff1e726f6f25f1aaa5a67280c
Harry <ex556615@gmail.com>
2025-08-21 15:01:28 +0800

    沒有登入不能觀看-1

--- project_f/client/app/user/edit/page.js
+++ project_f/client/app/user/edit/page.js
@@ -1,8 +1,22 @@
 "use client";
 
+import { useAuth } from "@/hooks/use-auth";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function UserEditPage() {
+  const { user, isLoading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/user/login"); // 導頁
+    }
+  }, [user, isLoading]);
+
+  if (isLoading || !user) return null;
+
   return (
     <div className="container py-3">
       <h1>編輯使用者</h1>
