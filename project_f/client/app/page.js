83fd355dfcafcaf4273a40becc2f97004c7466f1
Harry <ex556615@gmail.com>
2025-08-21 10:24:10 +0800

    page useAuth 條件渲染

--- project_f/client/app/page.js
+++ project_f/client/app/page.js
@@ -4,11 +4,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Logout from "./_components/logout";
 import Login from "./_components/login";
+import { useAuth } from "@/hooks/use-auth";
 
 export default function Home() {
+  const {user} = useAuth();
+
   return (
     <div className="container py-3">
-      <Login />
+      {user ? <Login /> : <Logout />}
     </div>
   );
 }
