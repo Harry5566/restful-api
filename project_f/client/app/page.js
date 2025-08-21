1ba59bd29491f6a3f87ffeecdba0f62ac297770e
Harry <ex556615@gmail.com>
2025-08-21 14:20:52 +0800

    CRUD LINK超連結

--- project_f/client/app/page.js
+++ project_f/client/app/page.js
@@ -2,16 +2,26 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
-import Logout from "./_components/logout";
-import Login from "./_components/login";
-import { useAuth } from "@/hooks/use-auth";
+import Link from "next/link";
 
 export default function Home() {
-  const { user, isLoading } = useAuth();
-
-  if (isLoading) {
-    return <div className="container py-3">Loading...</div>;
-  }
-
-  return <div className="container py-3">{user ? <Login /> : <Logout />}</div>;
+  return (
+    <div className="container py-3">
+      <Link className="btn btn-primary me-1 mb-1" href="/user/ben">
+        使用者的主頁(R)
+      </Link>
+      <Link className="btn btn-primary me-1 mb-1" href="/user/add">
+        新增使用者(C)
+      </Link>
+      <Link className="btn btn-primary me-1 mb-1" href="/user/edit">
+        修改使用者(U)
+      </Link>
+      <Link className="btn btn-primary me-1 mb-1" href="/user/login">
+        使用者登入
+      </Link>
+      <Link className="btn btn-primary me-1 mb-1" href="/user">
+        使用列表頁(R)
+      </Link>
+    </div>
+  );
 }
--- project_f/client/app/user/page.js
+++ project_f/client/app/user/page.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function UserPage() {
   return (
     <div className="container py-3">
-      <h1>使用者主頁</h1>
+      <h1>使用者列表頁</h1>
       <Link className="btn btn-primary" href="/">回首頁</Link>
     </div>
   );
